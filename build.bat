d83dcdf8332a75b915f326ca8197f41fad7245ae
tsai1247 <jeff29cc91079@gmail.com>
2024-06-03 12:32:01 +0800

    feat: reference 分離合併

--- build.bat
+++ build.bat
@@ -2,13 +2,13 @@ xelatex -synctex=1 -shell-escape -interaction=nonstopmode main.tex
 bibtex main.aux
 xelatex -synctex=1 -shell-escape -interaction=nonstopmode main.tex
 xelatex -synctex=1 -shell-escape -interaction=nonstopmode main.tex
-del *.aux 
-del *.log 
-del *.gz 
-del *.bbl 
-del *.blg 
-del *.lof 
-del *.lot 
-del *.toc 
-del *.out
+@REM del *.aux 
+@REM del *.log 
+@REM del *.gz 
+@REM del *.bbl 
+@REM del *.blg 
+@REM del *.lof 
+@REM del *.lot 
+@REM del *.toc 
+@REM del *.out
 
